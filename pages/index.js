5dbfafcf1c5b5aeef1c81e42e9d65a1beb558a54
Aditya R <a.raghumandiram@fortissecurity.com.au>
2021-02-08 17:17:59 +1100

    Modify text color on hover

--- pages/index.js
+++ pages/index.js
@@ -6,60 +6,59 @@ import Fade from "react-reveal/Fade";
 import { useState } from "react";
 
 export default function Home() {
-	const [descVisible, setDescVisible] = useState(false);
+  const [descVisible, setDescVisible] = useState(false);
+  const [active, setActive] = useState(false);
+  const handleActive = () => setActive(!active);
 
-	return (
-		<S.Container>
-			<Background />
-			<S.IconContainer>
-				{!descVisible && (
-					<S.Icon>
-						<Fade>
-							<img src={icon} />
-						</Fade>
-					</S.Icon>
-				)}
-				{descVisible && (
-					<>
-						<S.Icon>
-							<Slide right>
-								<img src={icon} />
-							</Slide>
-						</S.Icon>
-						<S.Description>
-							<Fade>
-								<p>
-									Growth of a product is a combination of strategies and not a
-									singular marketing function.
-								</p>
-								<p>
-									The core to this philosophy is building not for the world that
-									exists today but, the one we would love to live in the future.
-								</p>
-								<p>
-									Whether it’s something really simple or a complex unicorn
-									product we help build the brand story, technology and
-									data-driven growth stack setting your foundation.
-								</p>
-								<p>
-									We believe in crafting a scalable strategy that grows with you
-									and without us (long-term).
-								</p>
-							</Fade>
-						</S.Description>
-					</>
-				)}
-			</S.IconContainer>
-			<S.ContactContainer>
-				<p
-					style={{ cursor: "pointer" }}
-					onClick={() => setDescVisible(!descVisible)}
-				>
-					A FOUNDATION FOR SCALABLE GROWTH
-				</p>
-				<a href="mailto:run@northofzero.dev">CONTACT</a>
-				<p>2020 NORTH OF ZERO</p>
-			</S.ContactContainer>
-		</S.Container>
-	);
+  return (
+    <S.Container>
+      <Background />
+      <S.IconContainer>
+        <S.Icon onMouseEnter={handleActive} onMouseLeave={handleActive}>
+          <Fade>
+            <S.Brand active={active}>NORTH OF ZERO</S.Brand>
+          </Fade>
+        </S.Icon>
+        <S.Description>
+          <Fade when={descVisible}>
+            {descVisible && (
+              <div onMouseEnter={handleActive} onMouseLeave={handleActive}>
+                <S.Content active={active}>
+                  Growth of a product is a combination of strategies and not a
+                  singular marketing function.
+                </S.Content>
+                <S.Content active={active}>
+                  The core to this philosophy is building not for the world that
+                  exists today but, the one we would love to live in the future.
+                </S.Content>
+                <S.Content active={active}>
+                  Whether it’s something really simple or a complex unicorn
+                  product we help build the brand story, technology and
+                  data-driven growth stack setting your foundation.
+                </S.Content>
+                <S.Content active={active}>
+                  We believe in crafting a scalable strategy that grows with you
+                  and without us (long-term).
+                </S.Content>
+              </div>
+            )}
+          </Fade>
+        </S.Description>
+      </S.IconContainer>
+      <S.ContactContainer>
+        <S.ContentLink
+          active={active}
+          onMouseEnter={handleActive}
+          onMouseLeave={handleActive}
+          onClick={() => {
+            setDescVisible(!descVisible);
+          }}
+        >
+          A FOUNDATION FOR SCALABLE GROWTH
+        </S.ContentLink>
+        <a href="mailto:run@northofzero.dev">CONTACT</a>
+        <p>2020 NORTH OF ZERO</p>
+      </S.ContactContainer>
+    </S.Container>
+  );
 }
--- styles/styles.js
+++ styles/styles.js
@@ -1,101 +1,117 @@
 import styled from "@emotion/styled";
 import { mobile, tabletDown } from "../utils/mediaQuery";
 
+const colorTransition = "0.4s";
+
 export const Container = styled.div`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	align-content: center;
-	justify-content: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
 `;
 
 export const IconContainer = styled.div`
-	width: 100%;
-	height: 100%;
-	position: absolute;
-	z-index: 2;
-	justify-content: center;
-	display: flex;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 2;
+  justify-content: center;
+  display: flex;
 
-	${tabletDown} {
-		height: 80%;
-		flex-direction: column;
-	}
+  ${tabletDown} {
+    height: 80%;
+    flex-direction: column;
+  }
 `;
 
 export const Description = styled.div`
-	width: 70%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  position: absolute;
+  z-index: 3;
 
-	${tabletDown} {
-		width: 100%;
-		align-items: center;
-	}
+  ${tabletDown} {
+    width: 100%;
+    align-items: center;
+    justify-content: flex-end;
+  }
+`;
 
-	p {
-		width: 32rem;
-		color: #a6a6a6;
-		font-size: 1.1rem;
+export const Content = styled.p`
+  width: 32rem;
+  color: ${(props) => (props.active ? "#39FF14" : "#A6A6A6")};
+  transition: color ${colorTransition};
+  font-size: 1.1rem;
 
-		${tabletDown} {
-			font-size: 0.9rem;
-		}
+  ${tabletDown} {
+    font-size: 0.9rem;
+  }
 
-		${mobile} {
-			width: 20rem;
-		}
-	}
+  ${mobile} {
+    width: 20rem;
+  }
 `;
 
 export const Icon = styled.div`
-	display: flex;
-	width: 100%;
-	height: 100%;
-	justify-content: center;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 
-	img {
-		align-self: center;
-		width: calc(22rem + 15px);
-		height: 8.5rem;
+  ${tabletDown} {
+    align-items: flex-start;
+  }
+`;
 
-		${tabletDown} {
-			width: calc(18rem + 15px);
-			height: 7.5rem;
-		}
-	}
+export const Brand = styled.p`
+  width: calc(22rem + 15px);
+  height: 8.5rem;
+  font-family: Barcode;
+  font-size: 100px;
+  color: ${(props) => (props.active ? "#39FF14" : "#000000")};
+  transition: color ${colorTransition};
+
+  ${tabletDown} {
+    width: calc(18rem + 15px);
+    height: 7.5rem;
+  }
 `;
 
 export const ContactContainer = styled.div`
-	width: 100%;
-	height: 5rem;
-	position: absolute;
-	z-index: 2;
-	align-self: flex-end;
-	margin-bottom: 1.5rem;
+  width: 100%;
+  height: 5rem;
+  position: absolute;
+  z-index: 2;
+  align-self: flex-end;
+  margin-bottom: 1.5rem;
 
-	p {
-		margin: 0;
-		padding: 0;
+  p {
+    margin: 0;
+    padding: 0;
+    margin-left: 2.5rem;
+    line-height: 21px;
+    color: #a6a6a6;
+  }
 
-		margin-left: 2.5rem;
+  a {
+    margin: 0;
+    padding: 0;
+    margin-left: 2.5rem;
+    line-height: 21px;
+    text-decoration: none;
+    color: #a6a6a6;
+  }
+`;
 
-		line-height: 21px;
+export const ContentLink = styled.p`
+  color: ${(props) => (props.active ? "#39FF14" : "#a6a6a6")} !important;
+  transition: color ${colorTransition};
 
-		color: #a6a6a6;
-	}
-
-	a {
-		margin: 0;
-		padding: 0;
-
-		margin-left: 2.5rem;
-
-		line-height: 21px;
-
-		text-decoration: none;
-		color: #a6a6a6;
-	}
+  cursor: pointer;
 `;
